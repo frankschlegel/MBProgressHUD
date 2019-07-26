f3b4cd60ec1e978eb0808b6df12961d600897a1e
Frank Schlegel <frank.schlegel@digitalmasterpieces.com>
2019-07-26 15:38:17 +0200

    Updated Package.swift.

--- Package.swift
+++ Package.swift
@@ -4,14 +4,11 @@ import PackageDescription
 let package = Package(
     name: "MBProgressHUD",
     products: [
-        .library(name: "MBProgressHUD", targets: ["MBProgressHUD"])
+        .library(name: "MBProgressHUD", targets: ["MBProgressHUD"]),
     ],
     targets: [
         .target(
             name: "MBProgressHUD",
-            path: ".",
-            exclude: ["Demo"],
-            sources: ["MBProgressHUD.h", "MBProgressHUD.m"]
-        )
+            dependencies: [])
     ]
 )
